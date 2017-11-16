7039f98d3ca5f462beb785b7100dfe8567dfeda5
U-UL-101\ulab <ulab@UL-101.ad.clarkson.edu>
2017-11-16 13:08:29 -0500

    Made relay data structure for data facilitation

--- NetworkProject.py
+++ NetworkProject.py
@@ -2,23 +2,71 @@ import socket
 import sys
 
 
-class Receiver():
+class DeviceMappingRelay:
     def __init__(self):
-        pass
+        self.connected_transmitters = {}
+        self.connected_receivers = []
+        self.host_to_receiver_index = 2
+
+    def add_transmitter(self, host, port):
+        if host in self.connected_transmitters.keys():
+            if port in self.connected_transmitters[host].keys():
+                pass
+            else:
+                self.connected_transmitters[host][port] = []
+        else:
+            self.connected_transmitters[host] = {}
+            self.connected_transmitters[host][port] = {}
+
+    def add_receiver_subscription(self, host, port, receiver_index):
+        self.connected_transmitters[host][port].append(self.connected_receivers[receiver_index])
+
+    def remove_receiver_subscription(self, host, port, receiver_index):
+        if self.connected_receivers[receiver_index] in self.connected_transmitters[host][port]:
+            self.connected_receivers[host][port].remove(self.connected_receivers[receiver_index])
+
+
+class Receiver:
+    def __init__(self, ip, port):
+        self.receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.continue_listening = False
+        self.mapping = [] # Holds the current list of connected devices
+
+        self.ip = ip
+        self.port = port
 
     def listen_and_return(self):
         pass
 
     def run(self):
-        receive_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.continue_listening = True
+        self.receive_socket.bind((self.ip, self.port))
+        self.receive_socket.listen(5)
+        print "Socket listening"
+        while self.continue_listening:
+            c, addr = self.receive_socket.accept()
+            print "Got a connection from " + addr
+            print self.receive_socket.recv(2048)
 
 
-class Transmitter():
+class Transmitter:
     def __init__(self):
-        pass
+        self.transmit_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.continue_running = False
 
     def run(self):
-        pass
+        self.continue_running = True
+
+        while self.continue_running:
+            self.print_menu()
+            user_input = raw_input("Command: ")
+
+    def print_menu(self):
+        print "Enter: "
+        print "\tConnect: c [IP:port]"
+        print "\tPush Connection: p"
+        print "\tTransmit Message: [message]"
+        print "\tTransmit File: [filename]"
 
 
 class Relay():
@@ -30,22 +78,26 @@ class Relay():
 
 
 def print_help_menu():
-    pass
+    print "Help Menu"
+    print "\t--receive [host] [port]"
+    print "\t--relay [host] [port]"
 
 
 if __name__ == '__main__':
     min_arg_length = 2
-    print len(sys.argv)
 
     if sys.argv[1] == "--receive":
-        receive = Receiver()
-        receive.run()
+        if len(sys.argv) < 4:
+            print_help_menu()
+        else:
+            receive = Receiver(sys.argv[2], int(sys.argv[3]))
+            receive.run()
 
-    elif sys.argv[2] == "--transmit":
+    elif sys.argv[1] == "--transmit":
         transmit = Transmitter()
         transmit.run()
 
-    elif sys.argv[3] == "--relay":
+    elif sys.argv[1] == "--relay":
         relay = Relay()
         relay.run()
 
